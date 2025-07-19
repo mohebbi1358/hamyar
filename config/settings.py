a0302f8692813faf296119b913fa924d88a4908e
Mohebbi <mohebbi.cafe@gmail.com>
2025-07-19 16:37:32 +0330

    14040428-1

--- config/settings.py
+++ config/settings.py
@@ -117,7 +117,8 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # فایل‌های مدیا
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+#MEDIA_ROOT = BASE_DIR / 'media'
 
 # مدل کاربر سفارشی
 AUTH_USER_MODEL = 'accounts.User'
